04d39d6425f45af09360c041b993c27ee0ed41a6
Sander Ruscigno <alessander.ruscigno@gmail.com>
2019-07-03 13:41:29 -0300

    feat: ibm watson NLU

--- robots/text.js
+++ robots/text.js
@@ -2,6 +2,16 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apiKey
+
+var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+ 
+var nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
+
 async function robot(content){
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
@@ -46,6 +56,25 @@ async function robot(content){
                 text: sentence,
                 keywords: [],
                 images: []
+            })
+        })
+    }
+
+    async function fetchWatsoonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if (error) {
+                    throw error
+                }
+    
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
             })
         })
     }
